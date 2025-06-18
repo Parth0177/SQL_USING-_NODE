d5afb950b1abcea4408f47c467dc1f46d554fdc9
Parth Tiwari <parthtiwari670@gmail.com>
2025-06-18 14:51:54 +0530

    fix: change form method to POST for user edit functionality

--- student.js
+++ student.js
@@ -1,87 +1,102 @@
 const mysql = require('mysql2');
-const {faker} = require('@faker-js/faker');
-const express= require('express');
-const app= express();
-const PORT=5500;
-const path= require('path');
-const { log } = require('console');
+const { faker } = require('@faker-js/faker');
+const express = require('express');
+const app = express();
+const PORT = 5500;
+const path = require('path');
+const methodOverride = require('method-override');
 
-app.set ('view engine','ejs');
-app.set('views', path.join(__dirname,"/views"))
+app.use(methodOverride('_method'));
+app.use(express.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'));
 
+// MySQL connection
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    database: 'delta_app',
-    password:'Parth._017'
-})
+  host: 'localhost',
+  user: 'root',
+  database: 'delta_app',
+  password: 'Parth._017',
+});
 
-let getRandomUser = ()=> {
+// Function to generate a random user
+let getRandomUser = () => {
   return [
     faker.string.uuid(),
-    faker.internet.username(), // before version 9.1.0, use userName()
+    faker.internet.username(),
     faker.internet.email(),
     faker.internet.password(),
   ];
-}
+};
 
-
-
-//HOME ROUTE
-app.get('/',(req,res)=>{
-  let q= `SELECT count(*) FROM student`;
-  try {
-  connection.query(q, (err,result)=>{
-    if(err) throw err;
-    let count= result[0]["count(*)"];
-    console.log();
-    res.render('home.ejs',{count})
-  })
-} catch (err) {
-  console.log(err);
-  
-}
-})
-
-//SHOW USERS ROUTE
-app.get("/user",(req,res)=>{
-  let q= `SELECT * FROM student`;
-  try {
-  connection.query(q, (err,users)=>{
-    if(err) throw err;
-    //console.log(result);
-    res.render('user.ejs',{users});
-  })
-} catch (err) {
-  console.log(err);
-  
-}
+// HOME ROUTE
+app.get('/', (req, res) => {
+  let q = `SELECT COUNT(*) AS count FROM student`;
+  connection.query(q, (err, result) => {
+    if (err) {
+      console.log(err);
+      return res.status(500).send('Database Error');
+    }
+    let count = result[0].count;
+    res.render('home.ejs', { count });
+  });
 });
 
-//EDIT ROUTE
-app.get('/user/:id/edit',(req,res)=>{
-  let {id} = req.params;
-  let q = `SELECT * FROM student WHERE id= '${id}'`;
-  try {
-  connection.query(q, (err,result)=>{
-    if(err) throw err;
+// SHOW USERS ROUTE
+app.get('/user', (req, res) => {
+  let q = `SELECT * FROM student`;
+  connection.query(q, (err, users) => {
+    if (err) {
+      console.log(err);
+      return res.status(500).send('Database Error');
+    }
+    res.render('user.ejs', { users });
+  });
+});
+
+// EDIT ROUTE
+app.get('/user/:id/edit', (req, res) => {
+  let { id } = req.params;
+  let q = `SELECT * FROM student WHERE id = ?`;
+  connection.query(q, [id], (err, result) => {
+    if (err) {
+      console.log(err);
+      return res.status(500).send('Database Error');
+    }
     let user = result[0];
-    res.render('edit.ejs',{user});
-  })
-} catch (err) {
-  console.log(err);
-}
-  
-})
+    res.render('edit.ejs', { user });
+  });
+});
 
+// UPDATE ROUTE (PATCH)
+app.patch('/user/:id', (req, res) => {
+  let { id } = req.params;
+  let { password: formPass, username: newUsername } = req.body;
 
+  let q = `SELECT * FROM student WHERE id = ?`;
+  connection.query(q, [id], (err, result) => {
+    if (err) {
+      console.log(err);
+      return res.status(500).send('Database Error');
+    }
 
+    let user = result[0];
+    if (formPass !== user.password) {
+      return res.send('Wrong Password!');
+    }
 
+    let q2 = `UPDATE student SET username = ? WHERE id = ?`;
+    connection.query(q2, [newUsername, id], (err, updateResult) => {
+      if (err) {
+        console.log(err);
+        return res.status(500).send('Database Error');
+      }
+      res.redirect('/user');
+    });
+  });
+});
 
-
-
-app.listen(PORT,(req,res)=>{
-  console.log("app is running on port 5500");
-  
-})
-
+// Start Server
+app.listen(PORT, () => {
+  console.log(`App is running on port ${PORT}`);
+});
